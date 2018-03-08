4ff376aceb8ae4e33029292a554dee245148b73a
Balopy <lueng2yuan@163.com>
2018-03-08 10:12:47 +0800

    封装控件

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by 王春龙 on 2018/3/8.
+//  Copyright © 2018年 Balopy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by 王春龙 on 2018/3/8.
+//  Copyright © 2018年 Balopy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
